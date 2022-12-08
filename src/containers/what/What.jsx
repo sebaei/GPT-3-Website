6aa2d38feca82570bde68ac4acea2a856639063e
sebaei <47517850+sebaei@users.noreply.github.com>
2022-12-08 17:42:58 +0200

    Update Header and Add Brands

--- src/containers/what/What.jsx
+++ src/containers/what/What.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import { Feature } from "../../components";
 import "./what.css";
 
 const What = () => {
-  return <div>What</div>;
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
+  );
 };
 
 export default What;
